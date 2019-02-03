90281c19507793f652c90cc5af956ac6329aa4b0
Robert Schabacker <rschabac@stevens.edu>
2019-02-02 23:40:56 -0500

    display done?

--- snake.js
+++ snake.js
@@ -1,12 +1,18 @@
+//30 cols, 20 rows
+window.onLoad=function(){
+	//initialization code goes here
+};
+//TODO: make game loop using setInterval and clearInterval to pause
+	
+}
 function mkDebugCells(numRows, numCols){
 	let ar = [];
 	let gamespace = document.getElementById("gamespace");
-	for (r = 0;r<numRows;r++){
-		for (c = 0;c<numCols;c++){
+	for (c = 0;c<numRows;c++){
+		for (r = 0;r<numCols;r++){
 			let newCell = document.createElement("div");
 			newCell.setAttribute("id",r+","+c);
 			gamespace.appendChild(newCell);
 		}
 	}
 }
-//TODO: decide on # of rows and cols, set up more display properties in HTML/CSS
