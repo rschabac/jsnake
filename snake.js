103120947063fc2078363d4567d9a855bb7956f3
Robert Schabacker <rschabac@stevens.edu>
2019-06-09 16:56:30 -0400

    now using HTML attributes to record state

--- snake.js
+++ snake.js
@@ -1,12 +1,35 @@
-//30 cols, 20 rows
-window.onLoad=function(){
-	//initialization code goes here
-};
-//TODO: make game loop using setInterval and clearInterval to pause
-	
+const ROWS = 20;
+const COLS = 30;
+const DEFAULT_SQUARE = {
+    "age":0,
+    "food":false,
+    "color":"#f4f269"
 }
-function mkDebugCells(numRows, numCols){
-	let ar = [];
+const BOARD = Array(ROWS).fill(Array(COLS).fill(DEFAULT_SQUARE));//not using this anymore
+let headrow = 1, headcol = 1;
+let score = 5;
+let direction = -1;
+/* -1= not moving (only happens at the start of the game)
+ * 0 = up
+ * 1 = right
+ * 2 = down
+ * 3 = left
+ */
+window.addEventListener("load",function(){
+    initCells(ROWS,COLS);
+    let startCell = document.getElementById(headrow+","+headcol);
+    startCell.setAttribute("age","5");
+    startCell.setAttribute("food","false");
+    startCell.style.backgroundColor = getColor(5);
+
+});
+//TODO: make game loop using setInterval and clearInterval to pause
+
+function tick(){
+    
+}
+	
+function initCells(numRows, numCols){
 	let gamespace = document.getElementById("gamespace");
 	for (c = 0;c<numRows;c++){
 		for (r = 0;r<numCols;r++){
@@ -15,4 +38,8 @@ function mkDebugCells(numRows, numCols){
 			gamespace.appendChild(newCell);
 		}
 	}
+}
+function getColor(age){
+    if (age===0) return "";
+    return "hsl("+(360*(1-age/score))+",89%,78%)";
 }
