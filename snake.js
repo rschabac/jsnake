503a30bfcda50ac41ea97dbae9823e48d274365c
Robert Schabacker <rschabac@stevens.edu>
2019-06-10 16:50:18 -0400

    now starts with food particle

--- snake.js
+++ snake.js
@@ -5,7 +5,7 @@ const DEFAULT_SQUARE = {
     "food":false,
     "color":"#f4f269"
 }
-const BOARD = Array(ROWS).fill(Array(COLS).fill(DEFAULT_SQUARE));//not using this anymore
+//const BOARD = Array(ROWS).fill(Array(COLS).fill(DEFAULT_SQUARE));//not using this anymore
 let headrow = 1, headcol = 1;
 let score = 5;
 let direction = -1;
@@ -21,18 +21,52 @@ window.addEventListener("load",function(){
     startCell.setAttribute("age","5");
     startCell.setAttribute("food","false");
     startCell.style.backgroundColor = getColor(5);
+    let [foodR,foodC] = getAvailableCell();
+    let initialFoodCell = document.getElementById(foodR+","+foodC);
+    initialFoodCell.setAttribute("food","true");
+    initialFoodCell.style.backgroundColor = "#ff0000";
 
+    document.body.addEventListener("keydown", event => {
+        switch(event.keyCode){
+            case 38:
+                direction = 0;
+                break;
+            case 39:
+                direction = 1;
+                break;
+            case 40:
+                direction = 2;
+                break;
+            case 37:
+                direction = 3;
+                break;
+            case 32:
+                //TODO: implement pausing here
+        }
+    });
 });
 //TODO: make game loop using setInterval and clearInterval to pause
 
 function tick(){
-    
+    if(direction===-1)return;//???
+
 }
-	
+function getAvailableCell(){
+    //returns the x and y coordinates of a random cell that is not part of the snake
+    let r = Math.floor(Math.random()*ROWS);
+    let c = Math.floor(Math.random()*COLS);
+    let numTries = 0;
+    while(true){
+        if(numTries>10*ROWS*COLS) throw "VICTORY";
+        if (!document.getElementById(r+","+c).getAttribute("age")
+            || document.getElementById(r+","+c).getAttribute("age")==="0") return [r,c];
+        numTries++;
+    }
+}
 function initCells(numRows, numCols){
 	let gamespace = document.getElementById("gamespace");
-	for (c = 0;c<numRows;c++){
-		for (r = 0;r<numCols;r++){
+	for (r = 0;r<numRows;r++){
+		for (c = 0;c<numCols;c++){
 			let newCell = document.createElement("div");
 			newCell.setAttribute("id",r+","+c);
 			gamespace.appendChild(newCell);
